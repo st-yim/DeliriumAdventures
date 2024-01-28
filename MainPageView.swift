c126bf1ae1db348eec8a33fea619cfe5b1a20276
Steven <styim@seas.upenn.edu>
2024-01-27 18:36:56 -0800

    Initial Commit

---
+++ MainPageView.swift
@@ -0,0 +1,73 @@
+//
+//  MainPageView.swift
+//  DeliriumAdventures
+//
+//  Created by Steven Yim on 1/27/24.
+//
+
+import SwiftUI
+
+struct MainPageView: View {
+    @State private var isZoomed = false
+
+    var body: some View {
+        NavigationView {
+            ZStack {
+                RadialGradient(
+                            gradient: Gradient(colors: [Color.purple, Color("universal")]),
+                            center: .center,
+                            startRadius: 0,
+                            endRadius: 300
+                        )
+                .edgesIgnoringSafeArea(.all)
+                
+                VStack(spacing: 30) {
+                    // Text above the image
+                    Text("Ready?").underline()
+                        .font(.system(size: isZoomed ? 20 : 40, weight: .heavy))
+                        .foregroundColor(.white.opacity(0.8))
+
+                    
+                    // Image with constant zoom animation and navigation
+                    NavigationLink(destination: ContentView()) {
+                        Image("brain")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: isZoomed ? 95 : 80, height: isZoomed ? 95 : 80)
+                            .onAppear {
+                                // Start a timer to toggle the isZoomed state every second
+                                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                                    withAnimation {
+                                        isZoomed.toggle()
+                                    }
+                                }
+                            }
+                    }
+                    
+                    // Text below the image
+                    Text("Press the brain to begin!").underline()
+                        .font(.system(size: isZoomed ? 28 : 20, weight: .heavy))
+                        .foregroundColor(.white.opacity(0.8))
+
+                }
+                .padding(.top, -70)
+                .navigationTitle("Delirium Adventures")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        HStack {
+                            //Image(systemName: "house.fill")
+                            Text("Delirium Adventures")
+                                .font(.largeTitle)
+                                .fontWeight(.black)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MainPageView()
+}
